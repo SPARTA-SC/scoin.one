fb7a9a8a1117be9058b1540c145cf737e89ebe75
SPARTA <76834000+SPARTA-SC@users.noreply.github.com>
2021-01-01 16:33:47 +0800

    Add files via upload

--- puller/migration/20200910103420_create_table.go
+++ puller/migration/20200910103420_create_table.go
@@ -1,1 +1,39 @@
+package migration
 
+import (
+	"database/sql"
+	"github.com/jinzhu/gorm"
+	"github.com/pressly/goose"
+	"tezos_index/puller/models"
+)
+
+func init() {
+	goose.AddMigration(Up20200910103420, Down20200910103420)
+}
+
+func Up20200910103420(tx *sql.Tx) error {
+	// This code is executed when the migration is applied.
+	db, err := gorm.Open("mysql", tx)
+	if err != nil {
+		return err
+	}
+	err = db.AutoMigrate(
+		&models.Account{}, &models.Block{}, &models.Chain{}, &models.Flow{},
+		&models.Contract{}, &models.Op{}, &models.Supply{}, &models.BigMapItem{},
+		&models.Election{}, &models.Proposal{}, &models.Vote{}, &models.Ballot{},
+		&models.Income{}, &models.Right{}, &models.Snapshot{}, &models.HarvesterStatus{}).Error
+	return err
+}
+
+func Down20200910103420(tx *sql.Tx) error {
+	// This code is executed when the migration is rolled back.
+	db, err := gorm.Open("mysql", tx)
+	if err != nil {
+		return err
+	}
+	err = db.DropTableIfExists(&models.Account{}, &models.Block{}, &models.Chain{}, &models.Flow{},
+		&models.Contract{}, &models.Op{}, &models.Supply{}, &models.BigMapItem{},
+		&models.Election{}, &models.Proposal{}, &models.Vote{}, &models.Ballot{},
+		&models.Income{}, &models.Right{}, &models.Snapshot{}, &models.HarvesterStatus{}).Error
+	return err
+}
